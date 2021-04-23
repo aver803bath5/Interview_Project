0f1dbf0e4c64e119a25b31f6185b303d3bec3c85
aver803bath5 <aver803bath261@gmail.com>
2021-04-23 13:51:48 +0800

    feat(Models): generate domain model classes and dbcontext class from database

---
+++ Interview_Project/Models/PubInfo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace Interview_Project.Models
+{
+    public partial class PubInfo
+    {
+        public string PubId { get; set; }
+        public byte[] Logo { get; set; }
+        public string PrInfo { get; set; }
+
+        public virtual Publisher Pub { get; set; }
+    }
+}
