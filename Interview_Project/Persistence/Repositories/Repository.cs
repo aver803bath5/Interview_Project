f3cdbfc6bf14ffb4e1111d6daa8ec35f6c6b3a1c
aver803bath5 <aver803bath261@gmail.com>
2021-04-24 11:21:18 +0800

    feat(Persistence): add Repository class
    * Add Repository class to implement IRepository interface.

---
+++ Interview_Project/Persistence/Repositories/Repository.cs
@@ -0,0 +1,48 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Interview_Project.Core.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace Interview_Project.Persistence.Repositories
+{
+    public class Repository<TEntity, TContext> : IRepository<TEntity> where TEntity : class where TContext : DbContext
+    {
+        private readonly TContext _context;
+        private readonly DbSet<TEntity> _entities;
+
+        public Repository(TContext context)
+        {
+            _context = context;
+            _entities = _context.Set<TEntity>();
+        }
+
+        public async Task<IEnumerable<TEntity>> GetAllAsync()
+        {
+            return await _entities.ToListAsync();
+        }
+
+        public async Task<TEntity> GetAsync(int id)
+        {
+            return await _entities.FindAsync(id);
+        }
+
+        public async Task<TEntity> DeleteAsync(int id)
+        {
+            var entity = await _entities.FindAsync(id);
+            if (entity == null)
+                return entity;
+
+            _entities.Remove(entity);
+            await _context.SaveChangesAsync();
+
+            return entity;
+        }
+
+        public async Task<TEntity> AddAsync(TEntity entity)
+        {
+            await _entities.AddAsync(entity);
+            await _context.SaveChangesAsync();
+            return entity;
+        }
+    }
+}
