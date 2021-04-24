361dc6fc173caf35c77d0c2fe111b2370459dff3
aver803bath5 <aver803bath261@gmail.com>
2021-04-24 19:26:42 +0800

    refactor(PubsContext): extract employee configuration into separate file
    * Extract employee entity configuration into separate file to make the code in PubsContext.cs cleaner.

--- Interview_Project/Persistence/PubsContext.cs
+++ Interview_Project/Persistence/PubsContext.cs
@@ -35,6 +35,7 @@ namespace Interview_Project.Persistence
             modelBuilder.HasAnnotation("Relational:Collation", "SQL_Latin1_General_CP1_CI_AS");
 
             modelBuilder.ApplyConfiguration(new JobConfiguration());
+            modelBuilder.ApplyConfiguration(new EmployeeConfiguration());
 
             modelBuilder.Entity<Author>(entity =>
             {
@@ -125,75 +126,6 @@ namespace Interview_Project.Persistence
                     .WithMany()
                     .HasForeignKey(d => d.StorId)
                     .HasConstraintName("FK__discounts__stor___3C69FB99");
-            });
-
-            modelBuilder.Entity<Employee>(entity =>
-            {
-                entity.HasKey(e => e.EmpId)
-                    .HasName("PK_emp_id")
-                    .IsClustered(false);
-
-                entity.ToTable("employee");
-
-                entity.HasIndex(e => new {e.Lname, e.Fname, e.Minit}, "employee_ind")
-                    .IsClustered();
-
-                entity.Property(e => e.EmpId)
-                    .HasMaxLength(9)
-                    .IsUnicode(false)
-                    .HasColumnName("emp_id")
-                    .IsFixedLength(true);
-
-                entity.Property(e => e.Fname)
-                    .IsRequired()
-                    .HasMaxLength(20)
-                    .IsUnicode(false)
-                    .HasColumnName("fname");
-
-                entity.Property(e => e.HireDate)
-                    .HasColumnType("datetime")
-                    .HasColumnName("hire_date")
-                    .HasDefaultValueSql("(getdate())");
-
-                entity.Property(e => e.JobId)
-                    .HasColumnName("job_id")
-                    .HasDefaultValueSql("((1))");
-
-                entity.Property(e => e.JobLvl)
-                    .HasColumnName("job_lvl")
-                    .HasDefaultValueSql("((10))");
-
-                entity.Property(e => e.Lname)
-                    .IsRequired()
-                    .HasMaxLength(30)
-                    .IsUnicode(false)
-                    .HasColumnName("lname");
-
-                entity.Property(e => e.Minit)
-                    .HasMaxLength(1)
-                    .IsUnicode(false)
-                    .HasColumnName("minit")
-                    .IsFixedLength(true);
-
-                entity.Property(e => e.PubId)
-                    .IsRequired()
-                    .HasMaxLength(4)
-                    .IsUnicode(false)
-                    .HasColumnName("pub_id")
-                    .HasDefaultValueSql("('9952')")
-                    .IsFixedLength(true);
-
-                entity.HasOne(d => d.Job)
-                    .WithMany(p => p.Employees)
-                    .HasForeignKey(d => d.JobId)
-                    .OnDelete(DeleteBehavior.ClientSetNull)
-                    .HasConstraintName("FK__employee__job_id__48CFD27E");
-
-                entity.HasOne(d => d.Pub)
-                    .WithMany(p => p.Employees)
-                    .HasForeignKey(d => d.PubId)
-                    .OnDelete(DeleteBehavior.ClientSetNull)
-                    .HasConstraintName("FK__employee__pub_id__4BAC3F29");
             });
 
             modelBuilder.Entity<PubInfo>(entity =>
