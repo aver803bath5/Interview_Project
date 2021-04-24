fca991b8d5290ff20fec3f661f88565ecdaa0386
aver803bath5 <aver803bath261@gmail.com>
2021-04-24 15:46:28 +0800

    feat(JobRepository): encapsulate queries to query jobs
    * Encapsulate the queries for querying jobs with related employees into JobRepository class.

--- Interview_Project/Core/Repositories/IJobsRepository.cs
+++ Interview_Project/Core/Repositories/IJobsRepository.cs
@@ -1,9 +1,13 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
 using Interview_Project.Models;
 
 namespace Interview_Project.Core.Repositories
 {
     public interface IJobsRepository : IRepository<Job>
     {
-        
+        Task<IEnumerable<Job>> GetJobs(bool includeEmployee = true);
+
+        Task<Job> GetJob(short id, bool includeEmployee = true);
     }
 }
--- Interview_Project/Persistence/Repositories/JobRepository.cs
+++ Interview_Project/Persistence/Repositories/JobRepository.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
 using Interview_Project.Core.Repositories;
 using Interview_Project.Models;
 using Microsoft.EntityFrameworkCore;
@@ -6,8 +8,29 @@ namespace Interview_Project.Persistence.Repositories
 {
     public class JobRepository : Repository<Job, PubsContext>, IJobsRepository
     {
+        private readonly PubsContext _context;
+
         public JobRepository(PubsContext context) : base(context)
         {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Job>> GetJobs(bool includeEmployee = true)
+        {
+            if (!includeEmployee)
+                return await _context.Jobs.ToListAsync();
+
+            return await _context.Jobs.Include(j => j.Employees).ToListAsync();
+        }
+
+        public async Task<Job> GetJob(short id, bool includeEmployee = true)
+        {
+            if (!includeEmployee)
+                return await _context.Jobs.FindAsync(id);
+
+            return await _context.Jobs
+                .Include(j => j.Employees)
+                .SingleOrDefaultAsync(j => j.JobId == id);
         }
     }
 }
