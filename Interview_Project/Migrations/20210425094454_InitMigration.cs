d477a0e94211005b979e2ffe2d9c03685f7bb227
aver803bath5 <aver803bath261@gmail.com>
2021-04-25 19:10:42 +0800

    feat(Migrations): add first migration

---
+++ Interview_Project/Migrations/20210425094454_InitMigration.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Interview_Project.Migrations
+{
+    public partial class InitMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+        }
+    }
+}
