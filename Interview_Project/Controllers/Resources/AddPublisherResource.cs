07bca114e15f432b06fe845e0fdb452b76ca0d54
g8Man <aver803bath261@gmail.com>
2021-04-27 14:39:33 +0800

    feat(Resources): add validation to verify if required string property is empty string

--- Interview_Project/Controllers/Resources/AddPublisherResource.cs
+++ Interview_Project/Controllers/Resources/AddPublisherResource.cs
@@ -5,21 +5,13 @@ using System.Text.RegularExpressions;
 
 namespace Interview_Project.Controllers.Resources
 {
-    public class AddPublisherResource  : IValidatableObject
+    public class AddPublisherResource : IValidatableObject
     {
-        [Required]
-        public string PubId { get; set; }
-        [MaxLength(40)]
-        [Required]
-        public string PubName { get; set; }
-        [MaxLength(20)]
-        [Required]
-        public string City { get; set; }
-        [MaxLength(2)]
-        public string State { get; set; }
-        [MaxLength(30)]
-        [Required]
-        public string Country { get; set; }
+        [MaxLength(4)] [Required] public string PubId { get; set; }
+        [MaxLength(40)] [Required] public string PubName { get; set; }
+        [MaxLength(20)] [Required] public string City { get; set; }
+        [MaxLength(2)] public string State { get; set; }
+        [MaxLength(30)] [Required] public string Country { get; set; }
 
         public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
         {
@@ -28,6 +20,13 @@ namespace Interview_Project.Controllers.Resources
             {
                 yield return new ValidationResult("PubId is not match the pattern.");
             }
+
+            if (string.IsNullOrWhiteSpace(PubName))
+                yield return new ValidationResult("PubName cannot be empty");
+            if (string.IsNullOrWhiteSpace(City))
+                yield return new ValidationResult("City cannot be empty");
+            if (string.IsNullOrWhiteSpace(Country))
+                yield return new ValidationResult("Country cannot be empty");
         }
     }
 }
--- Interview_Project/Controllers/Resources/UpdatePublisherResource.cs
+++ Interview_Project/Controllers/Resources/UpdatePublisherResource.cs
@@ -1,14 +1,25 @@
+using System.Collections.Generic;
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 
 namespace Interview_Project.Controllers.Resources
 {
-    public class UpdatePublisherResource
+    public class UpdatePublisherResource : IValidatableObject
     {
         [MaxLength(40)] [Required] public string PubName { get; set; }
         [MaxLength(20)] [Required] public string City { get; set; }
         [MaxLength(2)] public string State { get; set; }
 
         [MaxLength(30)] [Required] public string Country { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (string.IsNullOrWhiteSpace(PubName))
+                yield return new ValidationResult("PubName cannot be empty");
+            if (string.IsNullOrWhiteSpace(City))
+                yield return new ValidationResult("City cannot be empty");
+            if (string.IsNullOrWhiteSpace(Country))
+                yield return new ValidationResult("Country cannot be empty");
+        }
     }
 }
